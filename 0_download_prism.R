ea2127f39fbe9a8bfdd3d7691bd3d38638fd5732
Jacob Idec <jacob.idec@ufl.edu>
2025-03-07 20:24:05 -0500

    added figures and updated gitignore, and refreshed "extra" qmds

--- 0_download_prism.R
+++ 0_download_prism.R
@@ -1,3 +1,3 @@
 library(prism)
-prism_set_dl_dir("D:/GitProjects/inat-daily-activity-analysis/data/prism")
+prism_set_dl_dir("data/prism")
 get_prism_monthlys(type = "tmean", year = 2014:2023, mon = 1:12, keepZip = FALSE)
